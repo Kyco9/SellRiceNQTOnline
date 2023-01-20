6b734bc1eb27afc1c9e40ec5ccc074b0f68715ee
Kyco9 <quangtrungben10@gmail.com>
2023-01-21 01:37:43 +0700

    first commit

---
+++ SellRiceOnline/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SellRiceOnline.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
